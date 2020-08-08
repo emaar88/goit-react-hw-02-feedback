2349ed48f4828624dc5f6a434c79020b7e20865c
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2020-08-09 02:52:15 +0300

    Updates

---
+++ precache-manifest.edd9c6e904b1a8ff04426d678cecc18a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1feee7141a57ea2c479564b1003c2c04",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "b2ac1ec7f5597359c318",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5c932c11.chunk.css"
+  },
+  {
+    "revision": "43eb8ea063346be6a8c2",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e05e33b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.e05e33b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ac1ec7f5597359c318",
+    "url": "/goit-react-hw-02-feedback/static/js/main.b56a79f5.chunk.js"
+  },
+  {
+    "revision": "3fe86fc60d65b2b21125",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.fec43dbe.js"
+  }
+]);
