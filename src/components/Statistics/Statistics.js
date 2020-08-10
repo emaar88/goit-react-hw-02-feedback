e5f619eece67d6bd59503989f388ed0ea8fa556f
Serhii Serdiuk <sergiy.emaar88@gmail.com>
2020-08-10 22:46:29 +0300

    to mimimaze count files

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classes from "./Statistics.module.css";
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul className={classes.list}>
+      <li className={classes.listItem}>Good: {good}</li>
+      <li className={classes.listItem}>Neutral: {neutral}</li>
+      <li className={classes.listItem}>Bad: {bad}</li>
+      <li className={classes.listItem}>Total: {total}</li>
+      {positivePercentage > 50 ? (
+        <li className={classes.listItem || classes.listItemTrue}>
+          Positive Percentage: {positivePercentage}%
+        </li>
+      ) : (
+        <li className={classes.listItem}>
+          Positive Percentage: {positivePercentage}%
+        </li>
+      )}
+    </ul>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
+
+export default Statistics;
